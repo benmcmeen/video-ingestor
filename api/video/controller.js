b6f0152d0fe45686af714af36f15932dd7519299
benmcmeen <fliptscript@gmail.com>
2018-04-09 09:19:53 -0500

    Updated message.

--- api/video/controller.js
+++ api/video/controller.js
@@ -1,5 +1,5 @@
 const index = async (req, res) => {
-  const result = await { data: 'This is a simple message.' };
+  const result = await { data: 'This is a simple message that\'s been updated.' };
   res.json(result);
 };
 
